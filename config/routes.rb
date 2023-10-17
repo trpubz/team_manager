6443e75ce67f15b9b4bc4a4b28343e1129a5139f
Taylor Pubins <tpubz@icloud.com>
2023-10-17 14:11:28 -0700

    Update Delete routes logic

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   get "/teams/", to: "teams#index"
   post "/teams/", to: "teams#create"
-  delete "/teams.:id", to: "teams#destroy"
+  delete "/teams/:id", to: "teams#destroy"
   get "/teams/new", to: "teams#new"
   get "/teams/:id", to: "teams#show"
   patch "teams/:id", to: "teams#update"
   get "/teams/:id/edit", to: "teams#edit"
+  # teamsplayerscontroller
   get "/teams/:id/players", to: "teams#players"  # handles sorted and filtered
   get "/teams/:id/players/new", to: "teams#create_player"
   post "/teams/:id/players", to: "teams#add_new_player"
